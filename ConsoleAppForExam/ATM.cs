b067aafbcca7787c5d30e0da14fcaf0cce80e381
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 19:57:19 +0200

    singleton

---
+++ ConsoleAppForExam/ATM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppForExam
+{
+    public class ATM
+    {
+        private static ATM _instance;
+        
+        private ATM()
+        {
+            Console.WriteLine("New Instance of ATM");
+        }
+        public static ATM Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new ATM();
+
+                }
+                return _instance;
+            }
+        }
+    }
+} 
