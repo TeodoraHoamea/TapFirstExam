417989107736699baf5b0d9b9c85cf513ae1127d
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 13:06:12 +0200

    Added class DepositAccount

---
+++ ConsoleAppForExam/DepositAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleAppForExam
+{
+    public class DepositAccount : Account
+    {
+        protected override decimal Calculatewithdraw(decimal ammount)
+        {
+            return 0m;
+        } 
+    }
+}
