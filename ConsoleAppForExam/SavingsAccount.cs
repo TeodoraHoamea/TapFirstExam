12053957ebc4250aad987faddc97c1743efe2342
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-04 13:07:36 +0200

    Added SavingsAccount class

---
+++ ConsoleAppForExam/SavingsAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppForExam
+{
+    public class SavingsAccount:Account
+    {
+        protected override decimal Calculatewithdraw(decimal ammount)
+        {
+            return ammount * 0.5m / 100m;
+        }
+    }
+}
